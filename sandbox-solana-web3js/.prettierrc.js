85178093fd7475795a6b4113b08021c6f2ea0414
linnefromice <linnefromice@gmail.com>
2024-12-03 22:52:39 +0900

    update: get tx

--- sandbox-solana-web3js/.prettierrc.js
+++ sandbox-solana-web3js/.prettierrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  ...require('gts/.prettierrc.json'),
+  ...require("gts/.prettierrc.json"),
   bracketSpacing: true,
   singleQuote: false,
-}
+};
--- sandbox-solana-web3js/src/getTx.ts
+++ sandbox-solana-web3js/src/getTx.ts
@@ -1,1 +1,17 @@
-// todo
+import { clusterApiUrl, Connection } from "@solana/web3.js";
+
+const main = async () => {
+  const connection = new Connection(clusterApiUrl("mainnet-beta"), "confirmed");
+  const txSig =
+    "3TZR1jg6dQmgx8376LpnebaRUiGjUvGwYiHVYAWE3eBgZ92aFarRbtjAbdYn9TevLnfAXRcib7m5Be2G5WSgzkbK";
+  // txSig = "3VZCqk372X55cfwaavSy85AqvgiLyb8RUc4uJbbgowuhDixP3RyTUGAzJ1kigMouVYBuJs3HU9GFdd7PyGw1nJhE"
+
+  // https://solana.com/docs/advanced/versions#how-to-set-max-supported-version
+  // const parsedTx = await connection.getTransaction(txSig, { maxSupportedTransactionVersion: 0 });
+  const tx = await connection.getParsedTransaction(txSig, {
+    maxSupportedTransactionVersion: 0,
+  });
+  console.dir(tx, { depth: null });
+};
+
+main().catch(console.error);
