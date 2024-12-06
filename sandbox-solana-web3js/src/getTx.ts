08da938b5ad74f277d10f76184cf3d524e6acd9d
linnefromice <linnefromice@gmail.com>
2024-12-06 12:51:45 +0900

    update

--- sandbox-solana-web3js/src/getTx.ts
+++ sandbox-solana-web3js/src/getTx.ts
@@ -11,7 +11,20 @@ const main = async () => {
   const tx = await connection.getParsedTransaction(txSig, {
     maxSupportedTransactionVersion: 0,
   });
-  console.dir(tx, { depth: null });
+  // console.dir(tx, { depth: null });
+  tx?.transaction.message.instructions.forEach((inst, index) => {
+    console.log(`Instruction ${index}:`);
+    console.log(`Program ID: ${inst.programId.toBase58()}`);
+    if (
+      inst.programId.toBase58() ==
+      "CPMMoo8L3F4NbTegBCKVNunggL7H1ZpdTHKxQB5qKP1C"
+    ) {
+      const _inst = inst as any;
+      console.log(`Data: ${Buffer.from(_inst.data).toString("base64")}`);
+      // https://solscan.io/account/CPMMoo8L3F4NbTegBCKVNunggL7H1ZpdTHKxQB5qKP1C#anchorProgramIdl
+      // https://solscan.io/tx/3TZR1jg6dQmgx8376LpnebaRUiGjUvGwYiHVYAWE3eBgZ92aFarRbtjAbdYn9TevLnfAXRcib7m5Be2G5WSgzkbK
+    }
+  });
 };
 
 main().catch(console.error);
