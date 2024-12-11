a6d1884d7ae6d9ad046a8c8415b6fbefd2a6e7b2
linnefromice <linnefromice@gmail.com>
2024-12-11 14:31:00 +0900

    update: program interface

--- program-global-counter/programs/program-global-counter/src/lib.rs
+++ program-global-counter/programs/program-global-counter/src/lib.rs
@@ -6,7 +6,22 @@ declare_id!("38iBYy8JAKBtjce1RAF2VpoRksUiFLghNDkmgDwJGMR6");
 pub mod program_global_counter {
     use super::*;
 
+    pub fn init_program(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    pub fn add(ctx: Context<Add>, data: Data) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    pub fn sub(ctx: Context<Sub>, data: Data) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
@@ -14,3 +29,12 @@ pub mod program_global_counter {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct Add {}
+
+#[derive(Accounts)]
+pub struct Sub {}
+
+#[account]
+pub struct Data {}
