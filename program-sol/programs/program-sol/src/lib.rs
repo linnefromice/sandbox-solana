799fa8bae588c5ce9fcb2d1215ad1b67997d9e7d
linnefromice <linnefromice@gmail.com>
2024-12-12 15:19:46 +0900

    update: funcs to init

--- program-sol/programs/program-sol/src/lib.rs
+++ program-sol/programs/program-sol/src/lib.rs
@@ -7,12 +7,14 @@ pub mod program_sol {
     use super::*;
 
     pub fn init_program(ctx: Context<InitializeProgram>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let bank = &mut ctx.accounts.bank;
+        bank.total_amount = 0;
         Ok(())
     }
 
     pub fn init_user(ctx: Context<InitializeUser>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let executor = &mut ctx.accounts.executor;
+        executor.total_amount = 0;
         Ok(())
     }
 
@@ -27,11 +29,49 @@ pub mod program_sol {
     }
 }
 
-#[derive(Accounts)]
-pub struct InitializeProgram {}
+#[account]
+pub struct Bank {
+    pub total_amount: u64,
+}
+impl Bank {
+    pub const MAX_SIZE: usize = 8;
+}
+
+#[account]
+pub struct UserStats {
+    pub total_amount: u64,
+}
+impl UserStats {
+    pub const MAX_SIZE: usize = 8;
+}
 
 #[derive(Accounts)]
-pub struct InitializeUser {}
+pub struct InitializeProgram<'info> {
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + Bank::MAX_SIZE,
+        seeds = [],
+        bump
+    )]
+    pub bank: Account<'info, Bank>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeUser<'info> {
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + UserStats::MAX_SIZE,
+    )]
+    pub executor: Account<'info, UserStats>, // アカウントを初期化
+    #[account(mut)]
+    pub signer: Signer<'info>, // ユーザーの署名者
+    pub system_program: Program<'info, System>, // 必須のシステムプログラム
+}
 
 #[derive(Accounts)]
 pub struct Deposit {}
