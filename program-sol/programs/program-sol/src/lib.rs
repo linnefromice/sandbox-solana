d8fee2bcc206e3be6b036656cafdbe4d0af3ec64
linnefromice <linnefromice@gmail.com>
2024-12-12 19:41:03 +0900

    update: deposit (temp)

--- program-sol/programs/program-sol/src/lib.rs
+++ program-sol/programs/program-sol/src/lib.rs
@@ -4,6 +4,11 @@ declare_id!("3FbrHy2HRjGVCDgQbzpoRaHAZUqyGLL3UVrhuX9U75tX");
 
 #[program]
 pub mod program_sol {
+    use anchor_lang::{
+        accounts::program,
+        system_program::{transfer, Transfer},
+    };
+
     use super::*;
 
     pub fn init_program(ctx: Context<InitializeProgram>) -> Result<()> {
@@ -19,29 +24,36 @@ pub mod program_sol {
     }
 
     pub fn deposit(ctx: Context<DepositOrWithdraw>, amount: u64) -> Result<()> {
-        let user_account = &mut ctx.accounts.executor;
+        let signer = &mut ctx.accounts.signer;
         let bank_account = &mut ctx.accounts.back;
+        let program_account = &mut ctx.accounts.system_program;
 
-        let inst = anchor_lang::solana_program::system_instruction::transfer(
-            &user_account.key(),
-            &bank_account.key(),
-            amount,
+        let cpi_ctx = CpiContext::new(
+            program_account.to_account_info(),
+            Transfer {
+                from: signer.to_account_info(),
+                to: bank_account.to_account_info(),
+            },
         );
-        anchor_lang::solana_program::program::invoke(
-            &inst,
-            &[
-                user_account.to_account_info(),
-                bank_account.to_account_info(),
-            ],
-        )?;
+        transfer(cpi_ctx, amount)?;
+        // let inst = anchor_lang::solana_program::system_instruction::transfer(
+        //     &signer.key(),
+        //     &bank_account.key(),
+        //     amount,
+        // );
+        // anchor_lang::solana_program::program::invoke(
+        //     &inst,
+        //     &[signer.to_account_info(), bank_account.to_account_info()],
+        // )?;
 
-        user_account.total_amount += amount;
+        ctx.accounts.executor.total_amount += amount;
         bank_account.total_amount += amount;
 
         Ok(())
     }
 
     pub fn withdraw(ctx: Context<DepositOrWithdraw>, amount: u64) -> Result<()> {
+        // let signer = &mut ctx.accounts.signer;
         let user_account = &mut ctx.accounts.executor;
         let bank_account = &mut ctx.accounts.back;
 
@@ -112,7 +124,10 @@ pub struct InitializeUser<'info> {
 #[derive(Accounts)]
 pub struct DepositOrWithdraw<'info> {
     #[account(mut)]
-    pub executor: Account<'info, UserStats>,
+    pub executor: Account<'info, UserStats>, // user's PDA
     #[account(mut, seeds = [], bump)]
-    pub back: Account<'info, Bank>,
+    pub back: Account<'info, Bank>, // program's PDA
+    #[account(mut)]
+    pub signer: Signer<'info>, // user's signer
+    pub system_program: Program<'info, System>, // 必須のシステムプログラム
 }
--- program-sol/tests/program-sol.ts
+++ program-sol/tests/program-sol.ts
@@ -2,6 +2,9 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { ProgramSol } from "../target/types/program_sol";
 import { assert } from "chai";
+import { BN } from "bn.js";
+
+const SOL_UNIT = 10 ** 9;
 
 describe("program-sol", () => {
   const provider = anchor.AnchorProvider.env();
@@ -48,5 +51,42 @@ describe("program-sol", () => {
     await callInitUser(keypair, user.publicKey);
     const userStats = await getUserStats(keypair.publicKey);
     assert(userStats.totalAmount.toNumber() == 0);
+  });
+  it(".deposit", async () => {
+    const keypair = anchor.web3.Keypair.generate();
+    const payer = keypair.publicKey;
+    await callInitUser(keypair, user.publicKey);
+
+    const __bankInfo = await program.provider.connection.getAccountInfo(pda);
+    console.log(
+      `before (before airdrop) > bankInfo.lamports: ${__bankInfo.lamports}`
+    );
+    const __payerInfo = await program.provider.connection.getAccountInfo(payer);
+    console.log(
+      `before (before airdrop) > payerInfo.lamports: ${__payerInfo.lamports}`
+    );
+
+    await provider.connection.requestAirdrop(payer, 4 * SOL_UNIT);
+
+    const _bankInfo = await program.provider.connection.getAccountInfo(pda);
+    console.log(`before > bankInfo.lamports: ${_bankInfo.lamports}`);
+    const _payerInfo = await program.provider.connection.getAccountInfo(payer);
+    console.log(`before > payerInfo.lamports: ${_payerInfo.lamports}`);
+
+    await program.methods
+      .deposit(new BN(3 * SOL_UNIT))
+      .accounts({ executor: payer })
+      .rpc();
+
+    const bank = await getBank(pda);
+    assert(bank.totalAmount.toNumber() == 3 * SOL_UNIT);
+    const bankInfo = await program.provider.connection.getAccountInfo(pda);
+    console.log(`after > bankInfo.lamports: ${bankInfo.lamports}`);
+    // assert(bankInfo.lamports == 3 * SOL_UNIT);
+    const userStats = await getUserStats(payer);
+    assert(userStats.totalAmount.toNumber() == 3 * SOL_UNIT);
+    const payerInfo = await program.provider.connection.getAccountInfo(payer);
+    console.log(`after > payerInfo.lamports: ${payerInfo.lamports}`);
+    // assert(payerInfo.lamports == 2 * SOL_UNIT);
   });
 });
