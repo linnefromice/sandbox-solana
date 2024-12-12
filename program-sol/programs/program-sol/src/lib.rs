d4e8f6d59edfc6a991ed0ceecaacefebe86ecf64
linnefromice <linnefromice@gmail.com>
2024-12-12 23:34:11 +0900

    update: withdraw & test

--- program-sol/programs/program-sol/src/lib.rs
+++ program-sol/programs/program-sol/src/lib.rs
@@ -4,10 +4,7 @@ declare_id!("3FbrHy2HRjGVCDgQbzpoRaHAZUqyGLL3UVrhuX9U75tX");
 
 #[program]
 pub mod program_sol {
-    use anchor_lang::{
-        accounts::program,
-        system_program::{transfer, Transfer},
-    };
+    use anchor_lang::system_program::{transfer, Transfer};
 
     use super::*;
 
@@ -25,7 +22,7 @@ pub mod program_sol {
 
     pub fn deposit(ctx: Context<DepositOrWithdraw>, amount: u64) -> Result<()> {
         let signer = &mut ctx.accounts.signer;
-        let bank_account = &mut ctx.accounts.back;
+        let bank_account = &mut ctx.accounts.bank;
         let program_account = &mut ctx.accounts.system_program;
 
         let cpi_ctx = CpiContext::new(
@@ -53,24 +50,47 @@ pub mod program_sol {
     }
 
     pub fn withdraw(ctx: Context<DepositOrWithdraw>, amount: u64) -> Result<()> {
-        // let signer = &mut ctx.accounts.signer;
-        let user_account = &mut ctx.accounts.executor;
-        let bank_account = &mut ctx.accounts.back;
+        let signer = &mut ctx.accounts.signer;
+        let bank_account = &mut ctx.accounts.bank;
+        let program_account = &mut ctx.accounts.system_program;
 
-        let inst = anchor_lang::solana_program::system_instruction::transfer(
-            &bank_account.key(),
-            &user_account.key(),
-            amount,
-        );
-        anchor_lang::solana_program::program::invoke(
-            &inst,
-            &[
-                bank_account.to_account_info(),
-                user_account.to_account_info(),
-            ],
-        )?;
+        **bank_account.to_account_info().try_borrow_mut_lamports()? -= amount;
+        **signer.to_account_info().try_borrow_mut_lamports()? += amount;
 
-        user_account.total_amount -= amount;
+        // ref: https://github.com/solana-developers/program-examples/blob/main/tokens/token-swap/anchor/programs/token-swap/src/instructions/deposit_liquidity.rs#L103-L111
+        // ref: https://solana.com/docs/programs/anchor/cpi
+        let bank_bumps: &[u8] = &[ctx.bumps.bank];
+        let bank_seeds = &[b"", bank_bumps];
+        let bank_signer_seeds = &[&bank_seeds[..]];
+
+        // NOTE: This is not working
+        //   "Transfer: `from` must not carry data",
+        //   "Program 11111111111111111111111111111111 failed: invalid program argument",
+        // let cpi_ctx = CpiContext::new(
+        //     program_account.to_account_info(),
+        //     Transfer {
+        //         from: bank_account.to_account_info(),
+        //         to: signer.to_account_info(),
+        //     },
+        // )
+        // .with_signer(bank_signer_seeds);
+        // transfer(cpi_ctx, amount)?;
+
+        // let inst = anchor_lang::solana_program::system_instruction::transfer(
+        //     &bank_account.key(),
+        //     &signer.key(),
+        //     amount,
+        // );
+        // anchor_lang::solana_program::program::invoke_signed(
+        //     &inst,
+        //     &[
+        //         bank_account.to_account_info(),
+        //         user_account.to_account_info(),
+        //     ],
+        //     bank_signer_seeds,
+        // )?;
+
+        ctx.accounts.executor.total_amount -= amount;
         bank_account.total_amount -= amount;
 
         Ok(())
@@ -126,7 +146,7 @@ pub struct DepositOrWithdraw<'info> {
     #[account(mut)]
     pub executor: Account<'info, UserStats>, // user's PDA
     #[account(mut, seeds = [], bump)]
-    pub back: Account<'info, Bank>, // program's PDA
+    pub bank: Account<'info, Bank>, // program's PDA
     #[account(mut)]
     pub signer: Signer<'info>, // user's signer
     pub system_program: Program<'info, System>, // 必須のシステムプログラム
--- program-sol/tests/program-sol.ts
+++ program-sol/tests/program-sol.ts
@@ -52,97 +52,180 @@ describe("program-sol", () => {
     const userStats = await getUserStats(keypair.publicKey);
     assert(userStats.totalAmount.toNumber() == 0);
   });
-  it(".deposit", async () => {
+  describe(".deposit / .withdraw", () => {
     const keypair1 = anchor.web3.Keypair.generate();
     const payer1 = keypair1.publicKey;
     const keypair2 = anchor.web3.Keypair.generate();
     const payer2 = keypair2.publicKey;
-    await callInitUser(keypair1, user.publicKey);
-    await callInitUser(keypair2, user.publicKey);
 
-    const sig = await provider.connection.requestAirdrop(
-      user.publicKey,
-      10 * SOL_UNIT
-    );
-    await provider.connection.confirmTransaction(sig);
+    before(async () => {
+      await callInitUser(keypair1, user.publicKey);
+      await callInitUser(keypair2, user.publicKey);
 
-    // NOTE: how to get balance
-    //   const accountInfo = await program.provider.connection.getAccountInfo(publicKey);
-    //   accountInfo.lamports
-    //
-    //   await program.provider.connection.getBalance(pda)
+      const sig = await provider.connection.requestAirdrop(
+        user.publicKey,
+        10 * SOL_UNIT
+      );
+      await provider.connection.confirmTransaction(sig);
+    });
 
-    const beforeBankInfo = await provider.connection.getAccountInfo(pda);
-    const beforeUserInfo = await provider.connection.getAccountInfo(
-      user.publicKey
-    );
+    it(".deposit", async () => {
+      // NOTE: how to get balance
+      //   const accountInfo = await program.provider.connection.getAccountInfo(publicKey);
+      //   accountInfo.lamports
+      //
+      //   await program.provider.connection.getBalance(pda)
 
-    await program.methods
-      .deposit(new BN(3 * SOL_UNIT))
-      .accounts({
-        executor: payer1,
-        signer: user.publicKey,
-      })
-      .rpc();
-
-    {
-      const bank = await getBank(pda);
-      assert(bank.totalAmount.toNumber() == 3 * SOL_UNIT);
-      const userStats1 = await getUserStats(payer1);
-      assert(userStats1.totalAmount.toNumber() == 3 * SOL_UNIT);
-      const userStats2 = await getUserStats(payer2);
-      assert(userStats2.totalAmount.toNumber() == 0 * SOL_UNIT);
-
-      const afterBankInfo = await provider.connection.getAccountInfo(pda);
-      const afterUserInfo = await provider.connection.getAccountInfo(
+      const beforeBankInfo = await provider.connection.getAccountInfo(pda);
+      const beforeUserInfo = await provider.connection.getAccountInfo(
         user.publicKey
       );
-      console.log(`before > bankInfo.lamports: ${beforeBankInfo.lamports}`);
-      console.log(`before > userInfo.lamports: ${beforeUserInfo.lamports}`);
-      console.log(`after > bankInfo.lamports: ${afterBankInfo.lamports}`);
-      console.log(`after > userInfo.lamports: ${afterUserInfo.lamports}`);
-      const diffBank = afterBankInfo.lamports - beforeBankInfo.lamports;
-      const diffUser = afterUserInfo.lamports - beforeUserInfo.lamports;
-      console.log(`diff > bankInfo.lamports: ${diffBank}`);
-      console.log(`diff > userInfo.lamports: ${diffUser}`);
-      assert(diffBank == 3 * SOL_UNIT);
-      // NOTE: consider fee
-      assert(diffUser > -3 * SOL_UNIT * 1.001);
-      assert(diffUser < -3 * SOL_UNIT * 1.0);
-    }
 
-    await program.methods
-      .deposit(new BN(6 * SOL_UNIT))
-      .accounts({
-        executor: payer2,
-        signer: user.publicKey,
-      })
-      .rpc();
+      {
+        const sig = await program.methods
+          .deposit(new BN(3 * SOL_UNIT))
+          .accounts({
+            executor: payer1,
+            signer: user.publicKey,
+          })
+          .rpc();
+        await provider.connection.confirmTransaction(sig);
 
-    {
-      const bank = await getBank(pda);
-      assert(bank.totalAmount.toNumber() == 9 * SOL_UNIT);
-      const userStats1 = await getUserStats(payer1);
-      assert(userStats1.totalAmount.toNumber() == 3 * SOL_UNIT);
-      const userStats2 = await getUserStats(payer2);
-      assert(userStats2.totalAmount.toNumber() == 6 * SOL_UNIT);
+        const bank = await getBank(pda);
+        assert(bank.totalAmount.toNumber() == 3 * SOL_UNIT);
+        const userStats1 = await getUserStats(payer1);
+        assert(userStats1.totalAmount.toNumber() == 3 * SOL_UNIT);
+        const userStats2 = await getUserStats(payer2);
+        assert(userStats2.totalAmount.toNumber() == 0 * SOL_UNIT);
 
-      const afterBankInfo = await provider.connection.getAccountInfo(pda);
-      const afterUserInfo = await provider.connection.getAccountInfo(
+        const afterBankInfo = await provider.connection.getAccountInfo(pda);
+        const afterUserInfo = await provider.connection.getAccountInfo(
+          user.publicKey
+        );
+        console.log(`before > bankInfo.lamports: ${beforeBankInfo.lamports}`);
+        console.log(`before > userInfo.lamports: ${beforeUserInfo.lamports}`);
+        console.log(`after > bankInfo.lamports: ${afterBankInfo.lamports}`);
+        console.log(`after > userInfo.lamports: ${afterUserInfo.lamports}`);
+        const diffBank = afterBankInfo.lamports - beforeBankInfo.lamports;
+        const diffUser = afterUserInfo.lamports - beforeUserInfo.lamports;
+        console.log(`diff > bankInfo.lamports: ${diffBank}`);
+        console.log(`diff > userInfo.lamports: ${diffUser}`);
+        assert(diffBank == 3 * SOL_UNIT);
+        // NOTE: consider fee
+        assert(diffUser > -3 * SOL_UNIT * 1.001);
+        assert(diffUser < -3 * SOL_UNIT * 1.0);
+      }
+
+      {
+        const sig = await program.methods
+          .deposit(new BN(6 * SOL_UNIT))
+          .accounts({
+            executor: payer2,
+            signer: user.publicKey,
+          })
+          .rpc();
+        await provider.connection.confirmTransaction(sig);
+
+        const bank = await getBank(pda);
+        assert(bank.totalAmount.toNumber() == 9 * SOL_UNIT);
+        const userStats1 = await getUserStats(payer1);
+        assert(userStats1.totalAmount.toNumber() == 3 * SOL_UNIT);
+        const userStats2 = await getUserStats(payer2);
+        assert(userStats2.totalAmount.toNumber() == 6 * SOL_UNIT);
+
+        const afterBankInfo = await provider.connection.getAccountInfo(pda);
+        const afterUserInfo = await provider.connection.getAccountInfo(
+          user.publicKey
+        );
+        console.log(`before > bankInfo.lamports: ${beforeBankInfo.lamports}`);
+        console.log(`before > userInfo.lamports: ${beforeUserInfo.lamports}`);
+        console.log(`after > bankInfo.lamports: ${afterBankInfo.lamports}`);
+        console.log(`after > userInfo.lamports: ${afterUserInfo.lamports}`);
+        const diffBank = afterBankInfo.lamports - beforeBankInfo.lamports;
+        const diffUser = afterUserInfo.lamports - beforeUserInfo.lamports;
+        console.log(`diff > bankInfo.lamports: ${diffBank}`);
+        console.log(`diff > userInfo.lamports: ${diffUser}`);
+        assert(diffBank == 9 * SOL_UNIT);
+        // NOTE: consider fee
+        assert(diffUser > -9 * SOL_UNIT * 1.001);
+        assert(diffUser < -9 * SOL_UNIT * 1.0);
+      }
+    });
+    it(".withdraw", async () => {
+      const beforeBankInfo = await provider.connection.getAccountInfo(pda);
+      const beforeUserInfo = await provider.connection.getAccountInfo(
         user.publicKey
       );
-      console.log(`before > bankInfo.lamports: ${beforeBankInfo.lamports}`);
-      console.log(`before > userInfo.lamports: ${beforeUserInfo.lamports}`);
-      console.log(`after > bankInfo.lamports: ${afterBankInfo.lamports}`);
-      console.log(`after > userInfo.lamports: ${afterUserInfo.lamports}`);
-      const diffBank = afterBankInfo.lamports - beforeBankInfo.lamports;
-      const diffUser = afterUserInfo.lamports - beforeUserInfo.lamports;
-      console.log(`diff > bankInfo.lamports: ${diffBank}`);
-      console.log(`diff > userInfo.lamports: ${diffUser}`);
-      assert(diffBank == 9 * SOL_UNIT);
-      // NOTE: consider fee
-      assert(diffUser > -9 * SOL_UNIT * 1.001);
-      assert(diffUser < -9 * SOL_UNIT * 1.0);
-    }
+
+      {
+        const sig = await program.methods
+          .withdraw(new BN(2 * SOL_UNIT))
+          .accounts({
+            executor: payer1,
+            signer: user.publicKey,
+          })
+          .rpc();
+        await provider.connection.confirmTransaction(sig);
+
+        const bank = await getBank(pda);
+        assert(bank.totalAmount.toNumber() == 7 * SOL_UNIT);
+        const userStats1 = await getUserStats(payer1);
+        assert(userStats1.totalAmount.toNumber() == 1 * SOL_UNIT);
+        const userStats2 = await getUserStats(payer2);
+        assert(userStats2.totalAmount.toNumber() == 6 * SOL_UNIT);
+
+        const afterBankInfo = await provider.connection.getAccountInfo(pda);
+        const afterUserInfo = await provider.connection.getAccountInfo(
+          user.publicKey
+        );
+        console.log(`before > bankInfo.lamports: ${beforeBankInfo.lamports}`);
+        console.log(`before > userInfo.lamports: ${beforeUserInfo.lamports}`);
+        console.log(`after > bankInfo.lamports: ${afterBankInfo.lamports}`);
+        console.log(`after > userInfo.lamports: ${afterUserInfo.lamports}`);
+        const diffBank = afterBankInfo.lamports - beforeBankInfo.lamports;
+        const diffUser = afterUserInfo.lamports - beforeUserInfo.lamports;
+        console.log(`diff > bankInfo.lamports: ${diffBank}`);
+        console.log(`diff > userInfo.lamports: ${diffUser}`);
+        assert(diffBank == -2 * SOL_UNIT);
+        // NOTE: consider fee
+        assert(diffUser < 2 * SOL_UNIT * 1.0);
+        assert(diffUser > 2 * SOL_UNIT * 0.999);
+      }
+
+      {
+        const sig = await program.methods
+          .withdraw(new BN(4 * SOL_UNIT))
+          .accounts({
+            executor: payer2,
+            signer: user.publicKey,
+          })
+          .rpc();
+        await provider.connection.confirmTransaction(sig);
+
+        const bank = await getBank(pda);
+        assert(bank.totalAmount.toNumber() == 3 * SOL_UNIT);
+        const userStats1 = await getUserStats(payer1);
+        assert(userStats1.totalAmount.toNumber() == 1 * SOL_UNIT);
+        const userStats2 = await getUserStats(payer2);
+        assert(userStats2.totalAmount.toNumber() == 2 * SOL_UNIT);
+
+        const afterBankInfo = await provider.connection.getAccountInfo(pda);
+        const afterUserInfo = await provider.connection.getAccountInfo(
+          user.publicKey
+        );
+        console.log(`before > bankInfo.lamports: ${beforeBankInfo.lamports}`);
+        console.log(`before > userInfo.lamports: ${beforeUserInfo.lamports}`);
+        console.log(`after > bankInfo.lamports: ${afterBankInfo.lamports}`);
+        console.log(`after > userInfo.lamports: ${afterUserInfo.lamports}`);
+        const diffBank = afterBankInfo.lamports - beforeBankInfo.lamports;
+        const diffUser = afterUserInfo.lamports - beforeUserInfo.lamports;
+        console.log(`diff > bankInfo.lamports: ${diffBank}`);
+        console.log(`diff > userInfo.lamports: ${diffUser}`);
+        assert(diffBank == -6 * SOL_UNIT);
+        // NOTE: consider fee
+        assert(diffUser < 6 * SOL_UNIT * 1.0);
+        assert(diffUser > 6 * SOL_UNIT * 0.999);
+      }
+    });
   });
 });
