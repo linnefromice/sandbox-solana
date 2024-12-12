e7be088f324677d4df954cbf5ae2823e56fda6eb
linnefromice <linnefromice@gmail.com>
2024-12-12 15:15:16 +0900

    update: interface

--- program-sol/programs/program-sol/src/lib.rs
+++ program-sol/programs/program-sol/src/lib.rs
@@ -6,11 +6,35 @@ declare_id!("3FbrHy2HRjGVCDgQbzpoRaHAZUqyGLL3UVrhuX9U75tX");
 pub mod program_sol {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn init_program(ctx: Context<InitializeProgram>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    pub fn init_user(ctx: Context<InitializeUser>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    pub fn deposit(ctx: Context<Deposit>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeProgram {}
+
+#[derive(Accounts)]
+pub struct InitializeUser {}
+
+#[derive(Accounts)]
+pub struct Deposit {}
+
+#[derive(Accounts)]
+pub struct Withdraw {}
