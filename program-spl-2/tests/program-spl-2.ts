712d8294d8dfd5fdbe5179949fc11c7beadb48f3
linnefromice <linnefromice@gmail.com>
2024-12-17 21:11:21 +0900

    update

--- program-spl-2/tests/program-spl-2.ts
+++ program-spl-2/tests/program-spl-2.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { ProgramSpl2 } from "../target/types/program_spl_2";
+import { assert } from "chai";
 
 describe("program-spl-2", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +9,15 @@ describe("program-spl-2", () => {
 
   const program = anchor.workspace.ProgramSpl2 as Program<ProgramSpl2>;
 
-  it("Is initialized!", async () => {
+  const [pda] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("root")],
+    program.programId
+  );
+
+  it(".init", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    await program.methods.init().rpc();
+    const rootState = await program.account.rootState.fetch(pda);
+    assert(rootState); // Check if account exists
   });
 });
